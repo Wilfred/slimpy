1f608396950de45e2b5777f8883baeda3e1b3e00
Wilfred Hughes <me@wilfred.me.uk>
2017-02-19 20:40:51 +0000

    Factor out eval

--- server.py
+++ server.py
@@ -3,19 +3,22 @@
 import traceback
 
 
+def eval_line(line):
+    try:
+        # If it's a single statement, eval, so we have a
+        # proper result.
+        code_obj = compile(line, '<string>', 'eval')
+    except SyntaxError:
+        code_obj = compile(line, '<string>', 'exec')
+
+    return eval(code_obj)
+
+
 def command_loop():
     while True:
         line = input("user> ")
         try:
-            try:
-                # If it's a single statement, eval, so we have a
-                # proper result.
-                code_obj = compile(line, '<string>', 'eval')
-            except SyntaxError:
-                code_obj = compile(line, '<string>', 'exec')
-
-            result = eval(code_obj)
-            print(result)
+            print(eval_line(line))
         except KeyboardInterrupt:
             return
         except:
